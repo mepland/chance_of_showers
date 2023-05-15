279a379b20fc1097b793c423f011fde6164f5160
Matthew Epland <matthewepland@gmail.com>
2023-05-14 23:29:15 -0400

    Worked on notebook

--- ana/exploratory_ana.py
+++ ana/exploratory_ana.py
@@ -24,8 +24,11 @@ from zoneinfo import ZoneInfo
 # from statsmodels.graphics.api import qqplot
 
 # %%
+import plotly.graph_objects as go
+
+# %%
 # plotting code from plotting.py
-# from plotting import plot_func
+from plotting import plot_func
 # marker_styles = ['o', '^', 'v', '+']
 
 # %%
@@ -53,6 +56,10 @@ time_fmt = "%H:%M:%S"
 datetime_fmt = f"{date_fmt} {time_fmt}"
 
 # %%
+# TODO use to draw reference hlines, get from config yaml shared between all scripts
+# full_pressure_value = 14000
+
+# %%
 # when t_start = t_start.replace(minute=t_start_minute, second=0, microsecond=0) fixed the issue of drifting seconds
 dt_end_of_drifting_seconds = datetime.datetime.strptime('2023-05-02 01:22:08', datetime_fmt).replace(tzinfo=ZoneInfo('UTC'))
 
@@ -71,10 +78,11 @@ def load_data(data_path, with_fname=False):
     dfp = pd.concat(dfp_list)
     dfp['datetime_utc'] = pd.to_datetime(dfp['datetime_utc'], utc=True, format=datetime_fmt)
     dfp['datetime_est'] = dfp['datetime_utc'].dt.tz_convert('US/Eastern')
+    # Add more date columns
     dfp['day_of_week_int'] = dfp['datetime_est'].dt.dayofweek
     dfp['day_of_week_str'] = dfp['datetime_est'].dt.day_name()
     # dfp['time'] = dfp['datetime_est'].dt.strftime(time_hm_fmt)
-    
+
     dfp = dfp.sort_values(['datetime_utc'], ascending=[True]).reset_index(drop=True)
 
     dfp_drift_seconds_records = dfp.loc[( (dt_end_of_drifting_seconds < dfp['datetime_utc']) & (dfp['datetime_utc'].dt.second != 0) )]
@@ -89,10 +97,103 @@ def load_data(data_path, with_fname=False):
 dfp_data = load_data(data_path)
 
 # %%
-dfp_data
+# dfp_data
 
 # %%
 dfp_data.dtypes
 
 # %%
 dfp_data['mean_pressure_value'].describe()
+
+# %% [markdown]
+# ### Minute Time Series
+
+# %%
+dfp_data.index = pd.DatetimeIndex(dfp_data['datetime_est']).tz_localize(None).to_period('T')
+
+# %%
+# TODO
+# convert to DatetimeIndex, with T (minute) fequency
+# create null rows between min and max datetime if they do not exist
+# dfp.index = pd.DatetimeIndex(dfp['datetime_est']).tz_localize(None)
+# dfp = dfp.asfreq('T')
+
+# %%
+dfp_data
+
+# %%
+dfp_data.loc[dfp_data['had_flow'].isnull()]
+
+# %%
+plot_objs_ts = {}
+plot_objs_ts['minutes'] = {'type': 'scatter',
+    'x': dfp_data['datetime_est'], 'y': dfp_data['mean_pressure_value'],
+    'c': f'C0', 'ms': '.', 'ls': '',
+    'label': None}
+
+# %%
+plot_func(plot_objs_ts, 'Minute', 'Mean Pressure Value', fig_size=(12, 8))
+
+# %%
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(x=dfp_data['datetime_est'], y=dfp_data['mean_pressure_value']))
+
+# Add range slider
+fig.update_layout(
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label="1h",
+                     step="hour",
+                     stepmode="todate"),
+                dict(count=12,
+                     label="12h",
+                     step="hour",
+                     stepmode="todate"),
+                dict(count=1,
+                     label="1d",
+                     step="day",
+                     stepmode="backward"),
+                dict(count=7,
+                     label="1w",
+                     step="day",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="1m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=6,
+                     label="6m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="YTD",
+                     step="year",
+                     stepmode="todate"),
+                dict(count=1,
+                     label="1y",
+                     step="year",
+                     stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
+        rangeslider=dict(
+            visible=True
+        ),
+        type="date"
+    )
+)
+
+
+
+fig.update_layout(
+    xaxis_title="Date",
+    yaxis_title="Pressure DAQ Value",
+)
+
+fig.update_xaxes(minor=dict(ticks="inside", showgrid=True))
+
+
+fig.show()
