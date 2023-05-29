f256892d0242d32ebdb1eaaf7c3960a301f31bd1
Matthew Epland <matthewepland@gmail.com>
2023-05-29 02:03:05 -0400

    style

--- daq/web/static/index.js
+++ daq/web/static/index.js
@@ -5,8 +5,11 @@ var had_flow_box_div = document.getElementById("had_flow_box");
 
 var pressure_value_normalized_gauge_div = document.getElementById("pressure_value_normalized_gauge");
 
-var pressure_value_normalized_polling_trace_div = document.getElementById("pressure_value_normalized_polling_trace");
-var had_flow_polling_trace_div = document.getElementById("had_flow_polling_trace");
+var mean_pressure_value_normalized_trace_div = document.getElementById("mean_pressure_value_normalized_trace");
+var past_had_flow_trace_div = document.getElementById("past_had_flow_trace");
+
+var pressure_value_normalized_trace_div = document.getElementById("pressure_value_normalized_trace");
+var had_flow_trace_div = document.getElementById("had_flow_trace");
 
 // config for all graphs
 var graph_config = {
@@ -25,23 +28,19 @@ function updateBoxes(t_str, i_polling, pressure_value, had_flow) {
 // gauge
 var pressure_value_normalized_gauge_data = [
   {
-    domain: { x: [0, 1], y: [0, 1] },
+    // domain: { x: [0, 1], y: [0, 1] },
+    domain: {x: [0.1, 0.9], y: [0.1, 1]},
     value: 0,
-    title: { text: "Pressure %" },
+    title: { text: "%" },
     type: "indicator",
-    mode: "gauge+number+delta",
+    mode: "gauge+number",
     delta: { reference: 100 },
     gauge: {
+      shape: "bullet",
       axis: { range: [0, 150] },
       steps: [
         { range: [0, 50], color: "lightsalmon" },
-        { range: [50, 70], color: "orange" },
       ],
-      threshold: {
-        line: { color: "red", width: 4 },
-        thickness: 0.75,
-        value: 40,
-      },
     },
   },
 ];
@@ -59,10 +58,26 @@ function updateGauge(pressure_value_normalized) {
 }
 
 // traces
+var mean_pressure_value_normalized_trace = {
+  x: [],
+  y: [],
+  name: "Mean Pressure %",
+  // mode: "lines+markers",
+  mode: "markers",
+  type: "line",
+};
+var past_had_flow_trace = {
+  x: [],
+  y: [],
+  name: "Past Had Flow",
+  // mode: "lines+markers",
+  mode: "markers",
+  type: "line",
+};
 var pressure_value_normalized_trace = {
   x: [],
   y: [],
-  name: "Polling Pressure %",
+  name: "Pressure %",
   // mode: "lines+markers",
   mode: "markers",
   type: "line",
@@ -70,13 +85,37 @@ var pressure_value_normalized_trace = {
 var had_flow_trace = {
   x: [],
   y: [],
-  name: "Polling Flow",
+  name: "Had Flow",
   // mode: "lines+markers",
   mode: "markers",
   type: "line",
 };
 
-var pressure_value_layout = {
+var pressure_value_mean_trace_layout = {
+  autosize: true,
+  title: {
+    text: "Mean Pressure %",
+  },
+  font: {
+    size: 14,
+    color: "#7f7f7f",
+  },
+  colorway: ["#B22222"],
+  margin: { t: 30, b: 20, l: 30, r: 20, pad: 0 },
+};
+var past_had_flow_trace_layout = {
+  autosize: true,
+  title: {
+    text: "Past Had Flow",
+  },
+  font: {
+    size: 14,
+    color: "#7f7f7f",
+  },
+  colorway: ["#00008B"],
+  margin: { t: 30, b: 20, l: 30, r: 20, pad: 0 },
+};
+var pressure_value_polling_trace_layout = {
   autosize: true,
   title: {
     text: "Pressure %",
@@ -86,44 +125,56 @@ var pressure_value_layout = {
     color: "#7f7f7f",
   },
   colorway: ["#B22222"],
-  //   width: 450,
-  //   height: 260,
   margin: { t: 30, b: 20, l: 30, r: 20, pad: 0 },
 };
-var had_flow_layout = {
+var had_flow_trace_layout = {
   autosize: true,
   title: {
-    text: "Flow",
+    text: "Had Flow",
   },
   font: {
     size: 14,
     color: "#7f7f7f",
   },
   colorway: ["#00008B"],
-  //   width: 450,
-  //   height: 260,
   margin: { t: 30, b: 20, l: 30, r: 20, pad: 0 },
 };
 
 Plotly.newPlot(
-  pressure_value_normalized_polling_trace_div,
-  [pressure_value_normalized_trace],
-  pressure_value_layout,
+  mean_pressure_value_normalized_trace_div,
+  [mean_pressure_value_normalized_trace],
+  pressure_value_mean_trace_layout,
   graph_config
 );
 Plotly.newPlot(
-  had_flow_polling_trace_div,
+  past_had_flow_trace_div,
+  [past_had_flow_trace],
+  past_had_flow_trace_layout,
+  graph_config
+);
+Plotly.newPlot(
+  pressure_value_normalized_trace_div,
+  [pressure_value_normalized_trace],
+  pressure_value_polling_trace_layout,
+  graph_config
+);
+Plotly.newPlot(
+  had_flow_trace_div,
   [had_flow_trace],
-  had_flow_layout,
+  had_flow_trace_layout,
   graph_config
 );
 
+let mean_pressure_value_normalized_x_array = [];
+let mean_pressure_value_normalized_y_array = [];
+let past_had_flow_x_array = [];
+let past_had_flow_y_array = [];
 let pressure_value_normalized_x_array = [];
 let pressure_value_normalized_y_array = [];
 let had_flow_x_array = [];
 let had_flow_y_array = [];
 
-function updateCharts(lineChartDiv, xArray, yArray, xValue, yValue, max_graph_points) {
+function updateChart(lineChartDiv, xArray, yArray, xValue, yValue, max_graph_points) {
   if (xArray.length >= max_graph_points) {
     xArray.shift();
   }
@@ -145,16 +196,38 @@ function updateCharts(lineChartDiv, xArray, yArray, xValue, yValue, max_graph_po
 function updateAll(jsonResponse) {
   let t_str = jsonResponse.t_est_str;
   let i_polling = parseInt(jsonResponse.i_polling);
-  let pressure_value_normalized = (100*parseFloat(jsonResponse.pressure_value_normalized)).toFixed(2);
   let pressure_value = parseInt(jsonResponse.pressure_value);
+  let pressure_value_normalized = (100*parseFloat(jsonResponse.pressure_value_normalized)).toFixed(2);
   let had_flow = parseInt(jsonResponse.had_flow);
+
+  let mean_pressure_value_normalized = (100*parseFloat(jsonResponse.mean_pressure_value_normalized)).toFixed(2);
+  let past_had_flow = parseInt(jsonResponse.past_had_flow);
 
   updateBoxes(t_str, i_polling, pressure_value, had_flow);
 
   updateGauge(pressure_value_normalized);
 
-  updateCharts(
-    pressure_value_normalized_polling_trace_div,
+  if (i_polling = 0) {
+    updateChart(
+      mean_pressure_value_normalized_trace_div,
+      mean_pressure_value_normalized_x_array,
+      mean_pressure_value_normalized_y_array,
+      t_str,
+      mean_pressure_value_normalized,
+      15 
+    );
+    updateChart(
+      past_had_flow_trace_div,
+      past_had_flow_x_array,
+      past_had_flow_y_array,
+      t_str,
+      past_had_flow,
+      15 
+    );
+  }
+
+  updateChart(
+    pressure_value_normalized_trace_div,
     pressure_value_normalized_x_array,
     pressure_value_normalized_y_array,
     i_polling,
@@ -162,8 +235,8 @@ function updateAll(jsonResponse) {
     60
   );
 
-  updateCharts(
-    had_flow_polling_trace_div,
+  updateChart(
+    had_flow_trace_div,
     had_flow_x_array,
     had_flow_y_array,
     i_polling,
