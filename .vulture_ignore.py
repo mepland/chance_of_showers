61148dc50ea096cc1c6b95b6622189edab23c98c
Matthew Epland <matthewepland@gmail.com>
2023-07-05 22:20:43 -0400

    Added vulture

---
+++ .vulture_ignore.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+# pylint: skip-file
+# type: ignore
+plot_func  # unused function (ana/plotting.py:33)
+frame  # unused variable (daq/daq.py:176)
+_.when_held  # unused attribute (daq/daq.py:238)
+_.when_released  # unused attribute (daq/daq.py:239)
+index  # unused function (daq/daq.py:343)
+disconnect  # unused function (daq/daq.py:405)
+_.show_server_banner  # unused attribute (daq/daq.py:426)
+args  # unused variable (daq/daq.py:426)
+_.when_held  # unused attribute (daq/switch_test.py:30)
+_.when_released  # unused attribute (daq/switch_test.py:31)
