ff82e6b09d20a89276cc6dfe258e81f597a7b39d
Matthew Epland <matthewepland@gmail.com>
2024-01-13 15:16:52 -0500

    disable duplicate code checks for subclasses, __int__ needs to be written out like this

--- utils/NHiTSModelWrapper.py
+++ utils/NHiTSModelWrapper.py
@@ -1,4 +1,4 @@
-# pylint: disable=invalid-name
+# pylint: disable=invalid-name,duplicate-code
 """Wrapper for NHiTS."""
 # pylint: enable=invalid-name
 
--- utils/TCNModelWrapper.py
+++ utils/TCNModelWrapper.py
@@ -1,4 +1,4 @@
-# pylint: disable=invalid-name
+# pylint: disable=invalid-name,duplicate-code
 """Wrapper for TCN."""
 # pylint: enable=invalid-name
 
