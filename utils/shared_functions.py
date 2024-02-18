3521f5705f1b407b5acf2bee1d78cc3d1325b115
Matthew Epland <matthewepland@gmail.com>
2024-02-17 20:51:36 -0500

    black

--- utils/shared_functions.py
+++ utils/shared_functions.py
@@ -16,7 +16,15 @@ from typing import TYPE_CHECKING, Any
 import holidays
 import pandas as pd
 
-__all__ = ["create_datetime_component_cols", "get_SoC_temp", "get_lock", "normalize_pressure_value", "read_secure_pickle", "rebin_chance_of_showers_time_series", "write_secure_pickle"]
+__all__ = [
+    "create_datetime_component_cols",
+    "get_SoC_temp",
+    "get_lock",
+    "normalize_pressure_value",
+    "read_secure_pickle",
+    "rebin_chance_of_showers_time_series",
+    "write_secure_pickle",
+]
 
 
 ################################################################################
